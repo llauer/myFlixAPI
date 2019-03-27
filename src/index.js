b3337a4166a037536a3a1fa3e0067f4881ca6a5d
Larry Lauer <larry.lauer.3d@gmail.com>
2019-03-26 22:44:50 -0700

    working on getting users working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 //jshint esversion:6
 
 const express = require('express');
-
+const uuid = require("uuid");
 const app = express();
+
+let users = [{
+  id: 1,
+  name: "Silent Bob",
+  email: "funnyman@comicbooks.org",
+  dateOfBirth: "05/16/1985",
+  favoriteMovies: ['Jay and Silent Bob']
+}];
+
+
 
 
 //trying a new way of logging. Disabled morgan for now.
@@ -10,6 +20,8 @@ app.use((req, res, next) => {
   console.log(`${new Date().toString()} => ${req.originalUrl}`);
   next();
 });
+
+
 
 //currently logging in UTC.
 // app.use(morgan('common'));
@@ -24,20 +36,35 @@ app.use(movieRoute);
 app.use(directorsRoute);
 app.use(genresRoute);
 app.use(usersRoute);
+app.use(express.json());
 // app.use(documentation);
 
+// logs requests to console
 app.use((req, res, next) => {
-  console.log(`${new Date().toString()}`);
+  console.log(`${new Date().toString()} => ${req.originalUrl}`);
   next();
 });
 
 //serves up all static pages.
 app.use(express.static('public'));
 
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.name) {
+    const message = 'Missing user data in request body';
+    res.status(400).send(message);
+  } else {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
 //catches all not found urls.
 app.all('*', function(req, res) {
   res.send('<h1>I am sorry I cannot find that.</h1>');
 });
+
 //error handeling
 app.use(function(err, req, res, next) {
   console.error(err.stack);
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,8 +4,30 @@ let express = require('express');
 
 let router = express.Router();
 
+// const uuid = require("uuid");
+
+// let users = [{
+//   id: 1,
+//   name: "Silent Bob",
+//   email: "funnyman@comicbooks.org",
+//   dateOfBirth: "05/16/1985",
+//   favoriteMovies: ['Jay and Silent Bob']
+// }];
+// router.post('/', (req, res) => {
+//   let newUser = req.body;
+//
+//   if (!newUser.name) {
+//     const message = 'Missing user data in request body';
+//     res.status(400).send(message);
+//   } else {
+//     newUser.id = uuid.v4();
+//     users.push(newUser);
+//     res.status(201).send(newUser);
+//   }
+// });
 router.get('/users', (req, res) => {
   res.send('You have requested the User Registration Page');
+  // res.json(users);
 });
 
 // params property on request.
@@ -17,4 +39,18 @@ router.get('/users/:name', (req, res) => {
 router.get('/users/:name/favorites', (req, res) => {
   res.send(`You have requested: ${req.params.name}'s favorites`);
 });
+
+// router.post('/users', (req, res) => {
+//   let newUser = req.body;
+//
+//   if (!newUser.name) {
+//     const message = "Missing name in request body";
+//     res.status(400).send(message);
+//   } else {
+//     newUser.id = uuid.v4();
+//     users.push(newUser);
+//     res.status(201).send(newUser);
+//   }
+// });
+
 module.exports = router;
