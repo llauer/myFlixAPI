f655f1788924d517c974089b0f05884404d30bb1
Larry Lauer <larry.lauer.3d@gmail.com>
2019-05-07 16:09:55 -0700

    update put to use findoneandupdate

--- src/routes/users.js
+++ src/routes/users.js
@@ -189,7 +189,7 @@ router.put('/users/:Username', passport.authenticate('jwt', { session: false }),
 
   var hashedPassword = Users.hashPassword(req.body.Password);
 
-  Users.update(
+  Users.findOneAndUpdate(
     { Username: req.params.Username },
     {
       $set: {
