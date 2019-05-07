b15f35eb5187fd352a1db438783588259a6ebbf6
Larry Lauer <larry.lauer.3d@gmail.com>
2019-05-07 12:48:15 -0700

    changed hashpassword

--- src/routes/users.js
+++ src/routes/users.js
@@ -96,7 +96,7 @@ router.post(
       return res.status(422).json({ errors: errors });
     }
 
-    var hashPassword = Users.hashPassword(req.body.Password);
+    var hashedPassword = Users.hashPassword(req.body.Password);
 
     Users.findOne({
       Username: req.body.Username
@@ -107,7 +107,7 @@ router.post(
         } else {
           Users.create({
             Username: req.body.Username,
-            Password: hashPassword,
+            Password: hashedPassword,
             Email: req.body.Email,
             Birthday: req.body.Birthday
           })
