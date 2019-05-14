5afac88d98bbdc39a9d180d68cc6715469c0352d
Larry Lauer <larry.lauer.3d@gmail.com>
2019-05-14 00:58:29 -0700

    built with parcel and added build script

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import React-DOM from 'react-dom';
+import ReactDOM from 'react-dom';
 
-// Import statement to indicate that you need to bundle `./index.scss
-
+// Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <div className='my-flix'>
-        <div>Good Morning</div>
+      <div className="my-flix">
+        <div>Good morning</div>
       </div>
     );
   }
 }
 
-// finds the root of the app
-const container = document.getElementsByClassName('app-container') [0];
+// Finds the root of your app
+const container = document.getElementsByClassName('app-container')[0];
 
 // Tells React to render your app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
