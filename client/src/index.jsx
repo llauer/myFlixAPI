12aa4294a609b642f09804e96f7fb072740674f2
Larry Lauer <larry.lauer.3d@gmail.com>
2019-05-16 23:58:01 -0700

    updated index.js to use mainview component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,22 +1,21 @@
+// client/src/index.jsx
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Import statement to indicate that you need to bundle `./index.scss`
+import { MainView } from './components/main-view/main-view';
+
+// Import statement to indicate that we need to bundle `./index.scss`
 import './index.scss';
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-    return (
-      <div className="my-flix">
-        <div>Good morning</div>
-      </div>
-    );
+    return <MainView />;
   }
 }
 
-// Finds the root of your app
+// Find the root of our app
 const container = document.getElementsByClassName('app-container')[0];
 
-// Tells React to render your app in the root DOM element
+// Tell React to render our app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
