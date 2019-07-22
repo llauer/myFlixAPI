03335fe5eb312ce83063cf8ec0df9791c04133ff
Larry Lauer <larry.lauer.3d@gmail.com>
2019-07-22 16:30:28 -0700

    Added movies-list.jsx

--- client-2/src/App.js
+++ client-2/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import  { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import moviesApp from './reducers/reducers';
-import { MainView } from './components/main-view/main-view';
+import  MainView  from './components/main-view/main-view';
 
 import './App.css';
 
