2d0f9f66dcb7de91bfafcfaba435da30de462219
Larry Lauer <larry.lauer.3d@gmail.com>
2019-07-22 15:35:52 -0700

    Changed filename on reducers and created store

--- client-2/src/App.js
+++ client-2/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-
+import  { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
 import { MainView } from './components/main-view/main-view';
 
 import './App.css';
 
+const store = createStore(moviesApp);
 class App extends Component {
   render() {
     return (
-      <MainView />
+      <Provider store={store}>
+        <MainView/>
+      </Provider>
+
     );
   }
 }
