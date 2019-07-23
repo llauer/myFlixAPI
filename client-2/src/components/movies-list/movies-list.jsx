f80513cdf9e54d83e140257513e79096376a3a7d
Larry Lauer <larry.lauer.3d@gmail.com>
2019-07-23 01:31:27 -0700

    Fixed form filter to work

--- client-2/src/components/movies-list/movies-list.jsx
+++ client-2/src/components/movies-list/movies-list.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { connect } from "react-redux";
-import {Row} from "react-bootstrap"
+import { Row } from "react-bootstrap";
 import { MovieCard } from "../movie-card/movie-card";
+import VisibilityFilterInput from "../visibility-filter-input/visibility-filter-input";
 
 const mapStateToProps = state => {
   const { movies, visibilityFilter, sortColumn } = state;
 
-  let sortedMovies = movies.concat().sort((a, b) => {
+  let moviesToShow = movies.concat().sort((a, b) => {
     if (a[sortColumn] < b[sortColumn]) return -1;
     if (a[sortColumn] > b[sortColumn]) return 1;
+    return 0;
   });
 
-  return { movies: sortedMovies };
+  if (visibilityFilter !== "") {
+    moviesToShow = moviesToShow.filter(m => m.Title.includes(visibilityFilter));
+  }
+
+  return { movies: moviesToShow };
 };
 
 function MoviesList(props) {
@@ -20,12 +26,25 @@ function MoviesList(props) {
   if (!movies) return <div className="main-view" />;
 
   return (
-    <Row>
-      {movies.map(movie => (
-        <MovieCard key={movie._id} movie={movie} />
-      ))}
-    </Row>
+    <div className="movies-list">
+      <Row>
+        <VisibilityFilterInput />
+
+        {movies.map(m => (
+          <MovieCard key={m._id} movie={m} />
+        ))}
+      </Row>
+    </div>
   );
 }
+
+//   return (
+//     <Row>
+//       {movies.map(movie => (
+//         <MovieCard key={movie._id} movie={movie} />
+//       ))}
+//     </Row>
+//   );
+// }
 
 export default connect(mapStateToProps)(MoviesList);
