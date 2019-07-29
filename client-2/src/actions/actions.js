2f8b7d051d51af1057ed10eb6da5b779f63f09d8
Larry Lauer <larry.lauer.3d@gmail.com>
2019-07-29 00:46:34 -0700

    Added actions for logged in user

--- client-2/src/actions/actions.js
+++ client-2/src/actions/actions.js
@@ -1,6 +1,7 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
 export const SET_SORT_COLUMN = 'SET_SORT_COLUMN';
+export const SET_LOGGED_IN_USER = 'SET_LOGGED_IN_USER';
 
 export function setMovies(value) {
   return { type: SET_MOVIES, value};
@@ -12,4 +13,8 @@ export function setFilter(value) {
 
 export function setSortColumn(value) {
   return { type: SET_SORT_COLUMN, value };
+}
+
+export function setLoggedInUser(value) {
+  return { type: SET_LOGGED_IN_USER, value };
 }
